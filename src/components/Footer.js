351289f27d0d97e0802ff7de912e61c424db3355
Eduardo Alba <eduardoandresalba@gmail.com>
2025-07-13 14:23:30 -0400

    Updated footer to reflect skillet studios

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,10 @@ function Footer() {
   return (
     <div>
       <p className="text-neutral-600 text-center text-xs md:text-base">
-        © 2023 Eduardo Alba. All Rights Reserved. Scuttle Bot is not officially
-        recognized or supported by Riot Games. League of Legends and Riot Games
-        are the property of Riot Games, Inc. Copyright © Riot Games, Inc.
+        © 2024 Skillet Studios. All Rights Reserved. Scuttle Bot is not
+        officially recognized or supported by Riot Games. League of Legends and
+        Riot Games are the property of Riot Games, Inc. Copyright © Riot Games,
+        Inc.
       </p>
     </div>
   );
